3bece3a87cfef00c95719e5148d9c7a618ad1c58
yonatanli <yonatanl@razorgrip.com>
2018-02-06 01:21:45 +0200

    add google sign in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, NgZone} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  gapi;
+
+  constructor(ngZone: NgZone) {
+    window['onSignIn'] = (user) => ngZone.run(() => this.onSignIn(user));
+  }
+
+  onSignIn(googleUser) {
+    console.log(googleUser.getBasicProfile());
+  }
+
+  signOut() {
+    const auth = gapi.auth2.getAuthInstance();
+    auth.signOut().then(() => console.log('SIGNED OUT'));
+  }
 }
